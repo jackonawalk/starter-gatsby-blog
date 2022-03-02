46cfa7e344a66451c825c436eb52061ab6bacd08
Jack Sellwood <jacksellwood@me.com>
2022-03-02 08:14:48 -0800

    upgrade to image cdn

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -22,7 +22,7 @@ class BlogPostTemplate extends React.Component {
           image={`http:${post.heroImage.resize.src}`}
         />
         <Hero
-          image={post.heroImage?.gatsbyImageData}
+          image={post.heroImage?.gatsbyImage}
           title={post.title}
           content={post.description?.childMarkdownRemark?.excerpt}
         />
@@ -84,7 +84,7 @@ export const pageQuery = graphql`
       publishDate(formatString: "MMMM Do, YYYY")
       rawDate: publishDate
       heroImage {
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
+        gatsbyImage(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
         resize(height: 630, width: 1200) {
           src
         }
